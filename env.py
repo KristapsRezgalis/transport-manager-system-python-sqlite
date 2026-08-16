79d01d7e644536b6fd04e1b98d4af99df31bab8c
Kristaps Rezgalis <kristaps.rezgalis@gmail.com>
2026-08-16 20:53:47 +0300

    Delete env.py

--- env.py
+++
@@ -1,3 +0,0 @@
-# Directory where are saved all transport order related documents
-base_dir_work = r"\\hippo.gemoss.lv\PUBLIC\LOGISTIKA\TMS"
-base_dir_home = r"C:\Users\KristapsRezgalis\Documents\TMS"
